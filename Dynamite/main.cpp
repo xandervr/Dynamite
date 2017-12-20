b57ec8b7ac835bd2b33539fd54627403be0e62df
xandervr <xandervanraemdonck@gmail.com>
2017-12-20 14:02:37 +0100

    Initial Commit

---
+++ Dynamite/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Dynamite
+//
+//  Created by Xander Van Raemdonck on 20/12/2017.
+//  Copyright © 2017 TnTap. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
